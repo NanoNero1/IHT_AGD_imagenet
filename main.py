a71a9a80e252cdd0ab84485cbc21db9c79b53105
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-17 21:00:11 +0200

    test

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import Subset
+from torchvision.transforms import Lambda
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -231,28 +232,86 @@ def main_worker(gpu, ngpus_per_node, args):
         train_dataset = datasets.FakeData(1281167, (3, 224, 224), 1000, transforms.ToTensor())
         val_dataset = datasets.FakeData(50000, (3, 224, 224), 1000, transforms.ToTensor())
     else:
-        traindir = os.path.join(args.data, 'train')
-        valdir = os.path.join(args.data, 'val')
+        #traindir = os.path.join(args.data, 'train')
+        #valdir = os.path.join(args.data, 'val')
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
+        
 
-        train_dataset = datasets.ImageFolder(
-            traindir,
-            transforms.Compose([
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                normalize,
-            ]))
+        timeOne = time.time()
+        #dataset = load_dataset("imagenet-1k")
 
-        val_dataset = datasets.ImageFolder(
-            valdir,
-            transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                normalize,
-            ]))
+
+        print("yes this did gather imagenet!")
+
+        timeTwo = time.time()
+
+        print(timeTwo - timeOne)
+
+        # Select the first row in the dataset
+        #sample = dataset['train'][0]
+
+        # Split up the sample into two variables
+        #datapt, label = sample['image'], sample['label']
+
+        #print(label)
+        #print(datapt)
+
+
+        ### SOURCE: https://medium.com/@ricodedeijn/image-classification-computer-vision-from-scratch-pt-3-9d5fbcf3c363
+        class MyDataset(torch.utils.data.Dataset):
+            def __init__(self, dataset):
+                # Loads the dataset that needs to be transformed
+                self.dataset = load_dataset("imagenet-1k", split=f"{dataset}")
+
+            def __getitem__(self, idx):
+                # Sample row idx from the loaded dataset
+                sample = self.dataset[idx]
+                
+                # Split up the sample example into an image and label variable
+                data, label = sample['image'], sample['label']
+
+                normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225])
+                
+                transform = transforms.Compose([
+                    transforms.Resize((256, 256)),  # Resize to size 256x256
+                    Lambda(lambda x: x.convert("RGB") if x.mode != "RGB" else x),  # Convert all images to RGB format
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),  # Transform image to Tensor object
+                    normalize
+
+                ])
+                
+                # Returns the transformed images and labels
+                return transform(data), torch.tensor(label)
+
+            def __len__(self):
+                return len(self.dataset)
+
+        # Call the class to populate variable train_set with the train data
+        train_dataset = MyDataset('train')
+        val_dataset = MyDataset('validation')
+
+        # train_dataset = datasets.ImageFolder(
+        #     traindir,
+        #     transforms.Compose([
+        #         transforms.RandomResizedCrop(224),
+        #         transforms.RandomHorizontalFlip(),
+        #         transforms.ToTensor(),
+        #         normalize,
+        #     ]))
+
+        # val_dataset = datasets.ImageFolder(
+        #     valdir,
+        #     transforms.Compose([
+        #         transforms.Resize(256),
+        #         transforms.CenterCrop(224),
+        #         transforms.ToTensor(),
+        #         normalize,
+        #     ]))
+        #train_dataset = 
+        #val_dataset = 
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
